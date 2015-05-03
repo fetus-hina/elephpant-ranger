508141bde6290e424bd40b12c5933c45311de8f6
AIZAWA Hina <hina@bouhime.com>
2015-05-03 18:50:29 +0900

    pypy-setup

---
+++ script/pypy-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu
+
+pushd $(cd $(dirname $0) && pwd)/../opt
+
+if [ ! -e pypy ]; then
+  wget -O pypy-download.tar.gz \
+    https://bitbucket.org/pypy/pypy/get/default.tar.gz 
+
+  mkdir pypy
+
+  tar -xvf pypy-download.tar.gz -C pypy --strip-components 1
+fi
+
+popd
